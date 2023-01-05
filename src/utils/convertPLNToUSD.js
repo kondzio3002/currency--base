3ea3f7b7cfdf79f63bca73cac9a8311490a92b36
Konrad Brożyna <kondzio3002@gmail.com>
2023-01-05 18:17:56 +0100

    Add tests

---
+++ src/utils/convertPLNToUSD.js
@@ -0,0 +1,23 @@
+export const convertPLNToUSD = (PLN) => {
+
+  const PLNtoUSD = PLN / 3.5;
+
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+    
+  if(typeof(PLN) === 'string' || typeof(PLN) === 'undefined'){
+    return NaN;
+  }
+
+  if(typeof PLN !== 'string' && typeof PLN !== 'number'){
+    return 'Error';
+  }
+
+  if(PLN < 0){
+    return formatter.format(0).replace(/\u00a0/g, ' ');
+  }
+
+  return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+}
